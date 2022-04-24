45ab924e6a79106d4879c080c1e53ad4bc2d9b4f
murad_0799 <murodbest19@gmail.com>
2022-04-24 11:59:25 +0500

    Register

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,32 @@
-import { createStore } from 'vuex'
+import {
+  createStore
+} from 'vuex'
+import axios from 'axios'
 
 export default createStore({
   state: {
+    user: null
   },
-  getters: {
-  },
+  getters: {},
   mutations: {
+    SET_USER_DATA(state, userData) {
+      state.user = userData
+      localStorage.setItem('user', JSON.stringify(userData))
+      axios.defaults.headers.common['Authorization'] = `Bearer ${userData.token}`
+    }
   },
   actions: {
+    register({
+      commit
+    }, credintials) {
+      return axios
+        .post('//localhost:3000/register', credintials)
+        .then(({
+          data
+        }) => {
+          commit('SET_USER_DATA', data)
+        })
+    }
   },
-  modules: {
-  }
+  modules: {}
 })
